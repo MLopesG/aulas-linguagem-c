d5157123c40efd2e058f52f4ca48ba8043afd7b1
Marcos Lopes <marcoslopesg7@gmail.com>
2020-02-07 21:58:43 -0400

    Add files via upload

---
+++ soma.c
@@ -0,0 +1,20 @@
+#include<stdio.h>;
+
+
+void main(){
+    int a;
+    int b;
+    int soma;
+
+
+    printf('Entre com primeiro numero');
+    scanf('%d',&a);
+    printf('Entre com segundo numero');
+    scanf('%d',&b);
+
+    soma = a + b;
+
+    printf("\nSua soma é:%d\n",soma);
+
+}
+
