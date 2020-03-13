fabeca2b133eddb0200da34245a5c32193c8f999
Marcos Lopes <marcoslopesg7@gmail.com>
2020-03-13 19:32:26 -0400

    Correção dos exercicios

--- Lista de Exercicio 3 Condicional/ex1.c
+++ Lista de Exercicio 3 Condicional/ex1.c
@@ -1,20 +1,23 @@
 #include<stdio.h>
 
-
 void main(){
     int idade;
 
-    printf("Entre com a idade: \n");
+    printf("Informe sua idade:");
     scanf("%d", &idade);
 
-    if(idade >= 5 && idade <= 7)
-        printf("Infatil A");
-    else if(idade >= 8 && idade <= 10)
-        printf("Infatil B");
-    else if(idade >= 11 && idade <= 13)
-        printf("Juvenil A");
-    else if(idade >= 14 && idade <=17)
-        printf("Juvenil B");
+    if(idade >= 5 && idade <=7)
+        printf("Infantil A");
     else
-        printf("Adulto");
+        if(idade >= 8 && idade <= 10)
+            printf("Infantil B");
+        else
+            if(idade >= 11 && idade  <= 13)
+                 printf("Juvenil A");
+            else
+                if(idade >= 14 && idade  <= 17)
+                     printf("Juvenil B");
+                else
+                   printf("Adulto");
 }
+
--- Lista de Exercicio 3 Condicional/ex3.c
+++ Lista de Exercicio 3 Condicional/ex3.c
@@ -1,12 +1,13 @@
+
 #include<stdio.h>
 
 void main(){
     float identificaco_aluno,n1,n2,n3,media_exercicios,ma;
 
-    printf("Informe o nÃºmero de identificaÃ§Ã£o do aluno:");
+    printf("Informe o número de identificação do aluno:");
     scanf("%f", &identificaco_aluno);
 
-    printf("Informe a mÃ©dia dos exercicios");
+    printf("Informe a média dos exercicios");
     scanf("%f", &media_exercicios);
 
     printf("Informe a primera nota:");
@@ -20,18 +21,22 @@ void main(){
 
     ma = (n1+n2*2+n3*3+media_exercicios)/7;
 
+
     if(ma >= 9)
         printf("Aluno %f  foi com o conceito A \n", identificaco_aluno);
-    else if(ma < 9 && ma >= 7.5)
-        printf("Aluno %f foi com o conceito B  \n", identificaco_aluno);
-    else if(ma < 7.5 && ma >= 6.0)
-        printf("Aluno %f foi com o conceito C  \n", identificaco_aluno);
-    else if(ma < 6.0 && ma >= 4.0)
-        printf("Aluno %f foi com o conceito D  \n", identificaco_aluno);        
-    else if(ma < 4.0)
-        printf("Aluno %f foi com o conceito E  \n", identificaco_aluno);    
     else
-        printf("Aluno %f foi com o conceito muito baixo  \n", identificaco_aluno);  
-        
-}
+        if(ma < 9 && ma >= 7.5)
+            printf("Aluno %f foi com o conceito B  \n", identificaco_aluno);
+        else
+         if(ma < 7.5 && ma >= 6.0)
+            printf("Aluno %f foi com o conceito C  \n", identificaco_aluno);
+          else
+             if(ma < 6.0 && ma >= 4.0)
+                printf("Aluno %f foi com o conceito D  \n", identificaco_aluno);
+             else
+                if(ma < 4.0)
+                    printf("Aluno %f foi com o conceito E  \n", identificaco_aluno);
+                else
+                    printf("Aluno %f foi com o conceito muito baixo  \n", identificaco_aluno);
 
+}
